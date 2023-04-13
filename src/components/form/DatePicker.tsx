7fe5489fc64f7b9fc84015167d2373f30da0e658
xApurvax <apurva.infynno@gmail.com>
2023-04-13 12:42:47 +0530

    fixed:profile submit function

--- src/components/form/DatePicker.tsx
+++ src/components/form/DatePicker.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { fixMeLater } from '../../utils/types'
 
 type DatePickerProps = {
   startDate?: Date;
-  setStartDate?: (e : Date) => void
+  setStartDate?: Dispatch<SetStateAction<DatePickerProps | undefined>>
 }
 
 export const CustomDatePicker = ({ startDate, setStartDate }: DatePickerProps) => {
   return (
-    <DatePicker selected={startDate} onChange={(date : Date) => setStartDate!(date)} />
+    <DatePicker selected={startDate} onChange={(date : DatePickerProps) => setStartDate!(date)} />
   )
 }
